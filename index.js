3f39840d5f7499ba433281f8b138eba8f3d11427
biswajeet787 <biswajeet780@gmail.com>
2026-05-28 12:51:42 +0530

    update

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const http = require('http');
 
 const server = http.createServer((req, res) => {
 
-res.write("CD Pipeline Working Successfully");
+res.write("CD Pipeline Working Successfully🎉🎉🎉🎉🎉🎉");
    res.end();
 
 });
